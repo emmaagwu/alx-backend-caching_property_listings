a1052f11cf155179bdbb60f20f695d19dd5b7e12
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-08-29 14:55:16 +0100

    Refactor property_list view to return data in a wrapped format and improve imports

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.views import View
 from .models import Property
 
-# Cache this view for 15 minutes
-@cache_page(60 * 15)
+@cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
-    properties = Property.objects.all().values("id", "title", "description", "price", "location")
-    return JsonResponse(list(properties), safe=False)
+    properties = Property.objects.all().values()  # returns QuerySet of dicts
+    return JsonResponse({
+        "data": list(properties)  # wrap results under "data"
+    }, safe=False)
