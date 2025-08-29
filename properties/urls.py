51c458a1fa4c3ad42ed66f04ea6301b25cd9f389
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-08-29 14:53:14 +0100

    Add properties URL routing and caching for property list view

---
+++ properties/urls.py
@@ -0,0 +1,7 @@
+# properties/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.property_list, name='property_list'),
+]
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+# Cache this view for 15 minutes
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all().values("id", "title", "description", "price", "location")
+    return JsonResponse(list(properties), safe=False)
